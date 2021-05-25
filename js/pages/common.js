d48888a29b29a72c80ff07d3371b0cf9294ca7d1
itsmeimtom <itsmeimtom@users.noreply.github.com>
2021-05-25 01:38:15 +0000

    jekyll build from Action 33cc8fc9fdf87303aa1e3d0446fa47f2411208b7

---
+++ js/pages/common.js
@@ -0,0 +1,41 @@
+document.body.classList.add('js-enabled');
+
+const hash = window.location.hash;
+
+// force self out of iframes, ish
+if (window.top !== window.self) { window.top.location.replace(window.self.location.href) };
+
+// open/closing the navigation
+const menu = document.querySelector('nav');
+
+function openMenu() {
+  menu.style.display = 'block';
+  document.getElementById('menu-toggle-open').style.display = 'none';
+  document.getElementById('menu-toggle-close').style.display = 'block';
+}
+
+function closeMenu() {
+  menu.style.display = 'none';
+  document.getElementById('menu-toggle-open').style.display = 'block';
+  document.getElementById('menu-toggle-close').style.display = 'none';
+}
+
+if(hash == '#menu') { openMenu(); }
+if(hash == '#no-menu') { closeMenu(); }
+
+// Contact alert box
+const emailAddr = `${atob('aGlA')}${atob('dG9tci5tZQ==')}`;
+if(document.getElementById('js-email')) {
+  document.getElementById('js-email').innerHTML = emailAddr;
+  document.getElementById('js-email').href = `mailto:${emailAddr}?subject=Hello!`;
+}
+
+if(hash == '#contact') { window.location = '/about'; }
+
+function loaderBar(percent) {
+  // this was a bad idea lol
+  // console.log(percent);
+  // document.getElementById('topbar').style.width = `${percent}%`;
+}
+
+// loaderBar(100);
